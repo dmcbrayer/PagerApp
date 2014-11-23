9c43e5913872fac028380d838c087295ad538555
dmcbrayer <dmcbrayer@gmail.com>
2014-11-23 13:29:36 -0500

    Bundled parse-ruby-client.  Added parse_init.rb to config/initializers.

---
+++ config/initializers/parse_init.rb
@@ -0,0 +1,5 @@
+require 'parse-ruby-client'
+
+Parse.init :application_id => "dLWsZ2vdt2oG3IIgWvO9mQzSzf7ant0clQpE0lS9",
+           :api_key        => "afEdKw0i63EBxJ3xA6yucQUJPuQ2TfuSkr94NTYJ",
+           :quiet           => false 
