a4b8ab45f907f673ad2298380371c66bdfb9042c
Daniel McBrayer <dmcbrayer@gmail.com>
2014-11-26 20:06:13 -0500

    Updated the message contents to include the sending user's info.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,21 +13,25 @@ class UsersController < ApplicationController
 
   def send_page
 
-    @user = User.find(params[:user_id])
-    @recipient = '+1' + @user.phone
-    @name = @user.full_name
+    @recipient_user = User.find(params[:user_id])
+    @recipient_number = '+1' + @recipient_user.phone
+    @recipient_name = @recipient_user.full_name
+
+    @sending_number = current_user.phone
+    @sending_name = current_user.full_name
+
 
     #Twilio actions
     @client = Twilio::REST::Client.new
 
     @client.account.messages.create({
       :from => '+16785828610', 
-      :to => @recipient, 
-      :body => 'Hey there ' + @name  
+      :to => @recipient_number, 
+      :body => "Message from #{@sending_name}:  Hey there #{@recipient_name}!  Call me back at #{@sending_number}."
     })
 
-
-  	flash[:notice] = "Page sent to #{@user.full_name}!"
+    #confirmation feedback kind of stuff
+  	flash[:notice] = "Page sent to #{@recipient_user.full_name}!"
   	puts "=================="
     puts "=   Page Sent!   ="
     puts "=================="
