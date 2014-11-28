36a54e626f5919f85480aa65d4277bfc3a04e009
Daniel McBrayer <dmcbrayer@gmail.com>
2014-11-28 16:01:42 -0500

    Fixed a syntax error in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-if Rails.env.development? do
+if Rails.env.development?
 	7.times {FactoryGirl.create(:user)}
 
 	User.create(email: "dmcbrayer@gmail.com", password: "password", password_confirmation: "password", phone: "2054227764", first_name: "Daniel", last_name: "McBrayer")
@@ -14,7 +14,7 @@ if Rails.env.development? do
 	Organization.create([{name: "Shelby Medical Center", variety: "Hospital"}, {name: "Trinity Medical Center", variety: "Hospital"},{name: "Fair Haven", variety: "Nursing Home"},{name: "Lakeshore", variety: "Rehabilitation Center"}])
 end
 
-if Rails.env.production? do
+if Rails.env.production?
 
 	User.create(email: "dmcbrayer@gmail.com", password: "password", password_confirmation: "password", phone: "2054227764", first_name: "Daniel", last_name: "McBrayer")
 
