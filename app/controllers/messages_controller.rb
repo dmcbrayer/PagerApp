6b044fd4546da1917fdf063a9cd9fa1555c65cd4
Daniel McBrayer <dmcbrayer@gmail.com>
2014-12-01 22:01:18 -0500

    Cleaned up a little bit.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,4 @@
 class MessagesController < ApplicationController
-  
-  #before_action :set_user, only: [:new, :create]
-
-  def new
-  end
-
-  #think through here about how you want to set this up if you're using a popover form to submit.  how are params getting passed through?  have a hidden field that contains the sending user's id and phone number.  Then have the form explicitly contain the message text.  Then the controller should append some text as well.  Form should also have limited characters.  Should have JS and AJAX validations that show up in the popover.
 
   def create
     @user = current_user
@@ -22,16 +15,13 @@ class MessagesController < ApplicationController
 
   private
 
-  def set_user
-  	@user = User.find(params[:id])
-  end
-
   def message_params
     params.require(:message).permit(:content, :user_id, :recipient)
   end
 
   def send_message(user_id, recipient, content)
 
+    #set up the required variables for the Twilio message
   	@recipient_user = User.find(recipient)
     @recipient_number = '+1' + @recipient_user.phone
     @recipient_name = @recipient_user.full_name
@@ -40,6 +30,8 @@ class MessagesController < ApplicationController
     @sending_number = @sending_user.phone
     @sending_name = @sending_user.full_name
 
+    @content = content + " Call back number:  #{@sending_number}.  Sent from #{@sending_name} via the Pager App."
+
 
     #Twilio actions
     @client = Twilio::REST::Client.new
@@ -47,7 +39,7 @@ class MessagesController < ApplicationController
     @client.account.messages.create({
       :from => '+16785828610', 
       :to => @recipient_number, 
-      :body => content
+      :body => @content
     })
 
     #confirmation feedback kind of stuff
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show] do
-  	resources :messages, only: [:new, :create]
+  	resources :messages, only: [:create]
   end
 
   resources :organizations
